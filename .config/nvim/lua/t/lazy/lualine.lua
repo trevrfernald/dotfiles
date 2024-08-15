9bb9efd435c6b87b07ef4c137cb3e1b8b3c85283
trevrfer_floqast <trevr.fernald@floqast.com>
2024-08-15 15:23:29 -0600

    rel path on lualine

--- .config/nvim/lua/t/lazy/lualine.lua
+++ .config/nvim/lua/t/lazy/lualine.lua
@@ -4,6 +4,9 @@ return {
 
     config = function()
         require('lualine').setup {
+            sections = {
+                lualine_c = { { 'filename', path = 1 } }  -- display file relative path
+            },
             options = { theme = 'onedark' }
         }
     end
