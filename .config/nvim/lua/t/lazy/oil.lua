ac44797159d92bd8a8ed60dad9265c04ae023ea8
trevrfer_floqast <trevr.fernald@floqast.com>
2024-09-12 11:43:58 -0600

    oil

---
+++ .config/nvim/lua/t/lazy/oil.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  view_options = {
+    -- Show files and directories that start with "."
+    show_hidden = false,
+    -- This function defines what is considered a "hidden" file
+    is_hidden_file = function(name, bufnr)
+      return vim.startswith(name, ".")
+    end,
+  }
+}
--- .config/nvim/lua/t/remap.lua
+++ .config/nvim/lua/t/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- old keymap fo netrw
 
 -- move highlighted lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
